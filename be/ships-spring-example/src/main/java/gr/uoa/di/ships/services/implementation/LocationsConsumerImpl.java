786e99355c1979f4e364455b575de117f1214ea0
DimitrisChrysos <jim.xrysos@gmail.com>
2025-06-04 14:59:58 +0300

    remove batching logic from vessel history data

--- be/ships-spring-example/src/main/java/gr/uoa/di/ships/services/implementation/LocationsConsumerImpl.java
+++ be/ships-spring-example/src/main/java/gr/uoa/di/ships/services/implementation/LocationsConsumerImpl.java
@@ -16,7 +16,6 @@ import gr.uoa.di.ships.services.interfaces.vessel.VesselHistoryDataService;
 import gr.uoa.di.ships.services.interfaces.vessel.VesselService;
 import gr.uoa.di.ships.services.interfaces.vessel.VesselStatusService;
 import gr.uoa.di.ships.services.interfaces.vessel.VesselTypeService;
-import jakarta.annotation.PreDestroy;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -79,7 +78,7 @@ public class LocationsConsumerImpl implements LocationsConsumer {
       sentToAnonymousUsers(jsonNodeToBeSent);
       sendToFilterCompliantRegisteredUsers(jsonNode, jsonNodeToBeSent);
       System.out.println("Sent message: " + jsonNodeToBeSent.toPrettyString());
-      handleBatches(jsonNode);
+      vesselHistoryDataService.saveVesselHistoryData(jsonNode);
     } catch (Exception e) {
       System.err.println(e.getMessage());
       log.error("Error while consuming Kafka message: {}", e.getMessage(), e);
@@ -150,34 +149,5 @@ public class LocationsConsumerImpl implements LocationsConsumer {
     tunedJsonNode.put("vesselType", Objects.nonNull(vesselType) ? vesselType.getName() : null);
     tunedJsonNode.put("status", vesselStatusService.getVesselStatusById(jsonNode.get("status").asLong()).getName());
     return tunedJsonNode;
-  }
-
-  @PreDestroy
-  public void onShutdown() {
-    synchronized (buffer) {
-      if (!buffer.isEmpty()) {
-        log.info("Flushing final batch of {} vessel history entries before shutdown.", buffer.size());
-        vesselHistoryDataService.saveVesselHistoryData(new ArrayList<>(buffer));
-        log.info("Saved final batch of {} vessel history entries to DB", buffer.size());
-        buffer.clear();
-        batchCount.set(0);
-      }
-    }
-  }
-
-  private void handleBatches(JsonNode jsonNode) {
-    buffer.add(jsonNode);
-    int currentCount = batchCount.incrementAndGet();
-    int batchSize = 20;
-    if (currentCount >= batchSize) {
-      synchronized (buffer) {
-        if (!buffer.isEmpty()) {
-          vesselHistoryDataService.saveVesselHistoryData(new ArrayList<>(buffer));
-          buffer.clear();
-          batchCount.set(0);
-          log.info("Saved {} vessel history entries to DB", batchSize);
-        }
-      }
-    }
   }
 }
